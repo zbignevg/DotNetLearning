d940cfb3d7bdec130ba9d631c8241e95f3de06e2
Zbignev.Grinevic <Zbignev.Grinevic@LTDEVNB13>
2022-11-07 18:37:35 +0200

    Add project files.

---
+++ ConsoleAppLearning/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
