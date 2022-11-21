4782e0c08048dfd523eb53fd83920c6ca9f54bc6
Zbignev Grinevič <Zbignev.Grinevic@oc.eu>
2022-11-21 20:28:31 +0200

    Logical Statements

---
+++ 002_LogicalStatements/Program.cs
@@ -0,0 +1,126 @@
+﻿using System.Threading.Tasks;
+
+namespace _002_LogicalStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter any number");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //if (number%2 == 0) Console.WriteLine("Number is even");
+            //else if (number % 2 < 0) Console.WriteLine("Number is negative");
+            //else Console.WriteLine($"Entered number is: {number}");
+
+
+
+
+            //Console.WriteLine("Enter group members amount");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //if (number == 1) Console.WriteLine("It's a solo player");
+            //else if (number == 2) Console.WriteLine("It's a duo");
+            //else if (number > 2 && number < 10) Console.WriteLine("It's a big band");
+            //else if (number > 10) Console.WriteLine("It's an chorus");
+            //else Console.WriteLine($"Wrong value entered.");
+
+
+
+
+            //Console.WriteLine("Enter how many hours you have worked");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //if (number < 160 && number > 0) Console.WriteLine($"Hours left: {160 - number}");
+            //else if (number == 160) Console.WriteLine("You've worked full-time");
+            //else if (number > 160) Console.WriteLine($"Your overtime hours {number - 160}");
+            //else Console.WriteLine($"Wrong value entered.");
+
+
+
+            //Console.WriteLine("Enter exam rate");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //switch (number)
+            //{
+            //    case 5:
+            //        Console.WriteLine("WEAK");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("SATISFACTORY");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("AVERAGE");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("OK");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("VERY GOOD");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("EXCELENT");
+            //        break;
+            //    case > 0: case <5:
+            //        Console.WriteLine("UNSATISFACTORY");
+            //        break;
+            //}
+
+
+            //int a = 5;
+            //int b = 6;
+
+            //if (++a == b | b++ == a++)
+            //{
+            //    b++;
+            //}
+
+            //Console.WriteLine($"{a} {b}");
+
+            //string day = "Wednesday";
+            //Console.WriteLine(day);
+
+            //Console.WriteLine("Temp in degrees C:");
+            //int degreesC = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Degrees K: {degreesC + 273.15}");
+
+            double weeklyPay = 600;
+            double dailyEarnings = weeklyPay / 7;
+            Console.WriteLine(dailyEarnings);
+
+        }
+
+        //class Person
+        //{
+        //    public string? name;
+
+        //    public void SayHello()
+        //    {
+        //        Console.WriteLine($"Hello, my name is: {name}");
+        //    }
+        //}
+
+
+        //        Taks #4
+
+        //Find, describe, and fix a bug in your app
+
+        //double weeklyPay = 600;
+        //        double dailyEarnings = weeklyPay / 7;
+        //        Console.WriteLine(dailyEarnings);
+
+
+        //        Task 3: Find and fix
+
+        //Console.WriteLine("Temp in degrees C:");
+        //        string degreesC = Console.ReadLine();
+        //        Console.WriteLine("Degrees K: " + (degreesC + 273.15));
+
+
+        //        Task 2:
+
+
+
+
+    }
+}
